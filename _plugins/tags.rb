0cb8ea31254423471799330e03dec8de0067a453
Thomas <92346180+thomaspradae@users.noreply.github.com>
2024-04-07 14:22:34 -0500

    Update

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -1,38 +1,38 @@
-module Jekyll
-  class TagPageGenerator < Generator
-    safe true
+# module Jekyll
+#   class TagPageGenerator < Generator
+#     safe true
 
-    def generate(site)
-      # Initialize an empty array to hold all documents
-      all_docs = []
+#     def generate(site)  
+#       # Initialize an empty array to hold all documents
+#       all_docs = []
     
-      # Add posts
-      all_docs.concat(site.posts.docs)
+#       # Add posts
+#       all_docs.concat(site.posts.docs)
     
-      # Add documents from each specified collection
-      site.collections.each do |name, collection|
-        all_docs.concat(collection.docs) if ['building', 'writing', 'notes'].include?(name)
-      end
+#       # Add documents from each specified collection
+#       site.collections.each do |name, collection|
+#         all_docs.concat(collection.docs) if ['building', 'writing', 'notes'].include?(name)
+#       end
     
-      # Now `all_docs` contains all the posts and documents from specified collections
-      tags = all_docs.flat_map { |doc| doc.data['tags'] || [] }.to_set
-      tags.each do |tag|
-        site.pages << TagPage.new(site, site.source, tag)
-      end
-    end
-  end
+#       # Now `all_docs` contains all the posts and documents from specified collections
+#       tags = all_docs.flat_map { |doc| doc.data['tags'] || [] }.to_set
+#       tags.each do |tag|
+#         site.pages << TagPage.new(site, site.source, tag)
+#       end
+#     end
+#   end
 
-  class TagPage < Page
-    def initialize(site, base, tag)
-      @site = site
-      @base = base
-      @dir  = File.join('tag', tag)
-      @name = 'index.html'
+#   class TagPage < Page
+#     def initialize(site, base, tag)
+#       @site = site
+#       @base = base
+#       @dir  = File.join('tag', tag)
+#       @name = 'index.html'
 
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
-      self.data['tag'] = tag
-      self.data['title'] = "Tag: #{tag}"
-    end
-  end
-end
+#       self.process(@name)
+#       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+#       self.data['tag'] = tag
+#       self.data['title'] = "Tag: #{tag}"
+#     end
+#   end
+# end
--- tag/incomplete.md
+++ tag/incomplete.md
@@ -11,10 +11,23 @@ layout: default
 {% endfor %}
 
 
-<h1>{{ tag }} Posts</h1>
+<h1>{{ tag }}</h1>
+ 
+<section class="posts">
+
+<ul class ="posts-ul">
+
+
+<p>{{ tagged_posts.size }} {{tag}} entr{% if tagged_posts.size != 1 %}ies{% else %}y{% endif %}</p>
 
 {% for post in tagged_posts %}
-  {{ post.title }}
+  <li class = "all-post-item">
+    <a class="all-post-item-title" href="{{ post.url }}">{{ post.title }}</a>
+    <time class="all-post-time" datetime="{{ post.date | date_to_xmlschema }}">{{ post.date | date: "%d-%m-%Y" }}</time>
+  </li>
 {% endfor %}
 
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+</ul>
+</section>
+
+
